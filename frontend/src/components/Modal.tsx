6344a09874789e2c795a341ebbb89343f89c83d0
ervin49 <ervinmarin49@gmail.com>
2026-08-11 00:13:39 +0300

    feat: add profile picture live preview and account deletion

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -2,15 +2,19 @@ import { X } from "lucide-react";
 
 export default function Modal({onClose}) {
     return (
-        <div
-            className="d-flex justify-content-center align-items-center position-fixed vw-100 vh-100 modal-ackdrop bg-opacity-25 opacity-100"
-        >
-            <button onClick={onClose} className="align-self-end bg-transparent border-0"><X/></button>
-            <div className="card p-4 d-flex align-items-center" style={{width: 400}}>
+        <div className="position-fixed vw-100 vh-100 d-block justify-content-center align-items-center"
+             style={{
+                 backdropFilter: "blur(5px)",
+                 zIndex: 2000,
+                 paddingTop: 400,
+                 paddingLeft: 600
+             }}>
+            <button onClick={onClose} className="align-self-end w-75 bg-transparent border-0"><X/></button>
+            <div className="card p-4 align-items-center " style={{width: 400, height: 200}}>
                 <h4 className="mb-3">Confirm account deletion</h4>
-                <>Are you sure you want to delete your account?</>
-                <>This action cannot be reversed.</>
-                <button className="btn btn-outline-danger rounded">Delete account</button>
+                <span>Are you sure you want to delete your account?</span>
+                <span>This action cannot be reversed.</span>
+                <button className="mt-3 btn btn-outline-danger rounded">Delete account</button>
             </div>
         </div>
     );
